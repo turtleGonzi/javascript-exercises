fd4cba8ba05897497e65b9b110fa449d920ebef8
turtleGonzi <nikolovjan@gmail.com>
2024-08-09 15:13:13 +0200

    Prevedeni z Celsia na Fahrenthain a zpet

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let celsius = (fahren - 32) * (5 / 9);
+  return (Number.isInteger(celsius)) ? celsius : Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahren = (celsius * (9 / 5) + 32)
+  return (Number.isInteger(fahren)) ? fahren : Number(fahren.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +13,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
