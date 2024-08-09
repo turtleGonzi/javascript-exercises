ddd3cc30ef94ad8c652ca5b42c5857b24041ea52
turtleGonzi <nikolovjan@gmail.com>
2024-08-09 12:59:21 +0200

    Splneni ukolu

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    return (num >= 0) ? str.repeat(num) : "ERROR";
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrs, ...nums) {
+    let newArr = [];
+    for(const arr of arrs){
+        if(!nums.includes(arr)){
+            newArr.push(arr);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
