b104fca67aa3b6b0f0f0c7ddb4dc2cb4cec9eeac
turtleGonzi <nikolovjan@gmail.com>
2024-08-09 13:46:39 +0200

    ukol

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((!Number.isInteger(num1) || !Number.isInteger(num2)) || (num1 < 0 || num2 < 0)){
+        return "ERROR";
+    }
 
+    if(num1 > num2) {
+        min = num2;
+        max = num1;
+    } else {
+        min = num1;
+        max = num2;
+    }
+
+    let sum = 0;
+
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
