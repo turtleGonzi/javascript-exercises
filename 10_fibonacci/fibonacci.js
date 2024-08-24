fb66b82e36ffa11aafca46c2a8550a81a804cf9f
turtleGonzi <nikolovjan@gmail.com>
2024-08-24 18:55:51 +0200

    Vytvoreni Fibonnaciho posloupnosti

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(numb) {
+    const num = (typeof numb === "number") ? numb : Number(numb);
+    if(num < 0) return 'OOPS';
+    const arr = [0];
+    for(let i = 1; i <= num; i++){
+        if (i===1) arr.push(1)
+        else {
+        arr.push(arr.at(-1) + arr.at(-2))
+        }
+    }
+    return arr.at(-1);
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
