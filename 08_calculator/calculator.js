e970899a1f126af419262a60c2eb06728546dc2d
turtleGonzi <nikolovjan@gmail.com>
2024-08-24 12:59:23 +0200

    vytvoreni funkce pro scitani odcitani nasobeni mocninu a faktorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	return numArr.reduce((acc, number) => {
+    return acc + number
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  return numArr.reduce((acc,num) => {
+    return acc * num
+  },1)
 };
 
-const power = function() {
-	
+const power = function(nem, exponent) {
+	return nem ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+  if(factor === 0) return 1
+  let sumFact = 1;
+	for(let i = factor; i >= 1; i--){
+    sumFact *= i;
+  }
+  return sumFact;
 };
 
 // Do not edit below this line
