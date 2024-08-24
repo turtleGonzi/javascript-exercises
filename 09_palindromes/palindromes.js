b097b82b7873a491abd047a3acc1ee79a1428e78
turtleGonzi <nikolovjan@gmail.com>
2024-08-24 13:56:11 +0200

    Kontroluje zda je slovo palindorm

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    const arrWord = word.toLowerCase().split('');
+    const filtredArr = arrWord.filter((letter)=>{
+        return (letter !== "!" && letter !== "?" && letter !== "." && letter !== " " && letter !== ",") ? letter : '';
+    })
+    const filtredWord = filtredArr.join('');
+    const backwardsWord = filtredArr.reverse().join('');
+    return (filtredWord === backwardsWord) ? true : false;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
