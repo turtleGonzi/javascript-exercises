d67195245fb2bfaee3a6ddbbb25e18cdd1b02da0
turtleGonzi <nikolovjan@gmail.com>
2024-08-24 21:34:25 +0200

    Vytvoreni funkce pro zjistovani nejstarsiho cloveka

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(ppl) {
+    const modifPpl = ppl.map((person)=>{
+        if(!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
+        return  ({[`name`]: person.name,[`year`]: person.yearOfDeath - person.yearOfBirth}); 
+    });
+  
+  
+    const sortPpl = modifPpl.sort((a, b) => {
+      return b.year - a.year;
+    });
+  
+    return sortPpl[0];
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
